d0c970ffd6e6378aa155736287b2ac96b304d7f0
dong53358 <dong53358@naver.com>
2022-09-12 13:26:25 +0900

    routing and firebase setting

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <h1 className={styles.title}>
+        <Link to="/">My Diary</Link>
+      </h1>
+      <ul className={styles.list_nav}>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/signup">회원가입</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
