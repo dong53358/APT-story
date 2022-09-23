5e997fad661e12e6532c9b7840687ef70338bb9f
dong53358 <dong53358@naver.com>
2022-09-23 19:17:28 +0900

    css edit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import "normalize.css";
 import { AuthContextProvider } from "./context/AuthContext";
+import "normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
